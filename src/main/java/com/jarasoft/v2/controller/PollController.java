56f1160d22ee0f4f8a7b141ecbe2783b8df3c63d
Daniel Jaramillo <jaramo92@gmail.com>
2022-05-05 14:46:17 -0500

    agregar version 2(hipermedia,paginacion y clasificacion)

--- src/main/java/com/jarasoft/v2/controller/PollController.java
+++ src/main/java/com/jarasoft/v2/controller/PollController.java
@@ -1,13 +1,19 @@
-package com.jarasoft.v1.controller;
+package com.jarasoft.v2.controller;
 
 import com.jarasoft.domain.Poll;
 import com.jarasoft.dto.error.ErrorDetails;
 import com.jarasoft.exception.ResourceNotFoundException;
 import com.jarasoft.repository.PollRepository;
+import com.jarasoft.util.PollModelAssembler;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiResponse;
 import io.swagger.annotations.ApiResponses;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PagedResourcesAssembler;
+import org.springframework.hateoas.EntityModel;
+import org.springframework.hateoas.PagedModel;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -19,22 +25,29 @@ import java.net.URI;
 import java.util.Optional;
 
 
-@RestController("pollControllerV1")
-@RequestMapping("/v1")
+@RestController("pollControllerV2")
+@RequestMapping("/v2")
 @Api(value = "polls")
 public class PollController {
+    private final PollModelAssembler pollModelAssembler;
     private final PollRepository pollRepository;
+    private final PagedResourcesAssembler<Poll> pollPagedResourcesAssembler;
 
-    public PollController(PollRepository pollRepository) {
+    public PollController(PollModelAssembler pollModelAssembler, PollRepository pollRepository, PagedResourcesAssembler<Poll> pollPagedResourcesAssembler) {
+        this.pollModelAssembler = pollModelAssembler;
         this.pollRepository = pollRepository;
+        this.pollPagedResourcesAssembler = pollPagedResourcesAssembler;
     }
 
     @ApiOperation(value = "Retrieves all the polls", response = Poll.class, responseContainer = "List")
     @ApiResponses(value = {@ApiResponse(code = 200, message = "Ok"),
             @ApiResponse(code = 500, message = "Server error", response = ErrorDetails.class)})
     @GetMapping("/polls")
-    public ResponseEntity<Iterable<Poll>> getAllPulls() {
-        return new ResponseEntity<>(pollRepository.findAll(), HttpStatus.OK);
+    public ResponseEntity<PagedModel<EntityModel<Poll>>> getAllPolls(Pageable pageable) {
+        Page<Poll> polls = pollRepository.findAll(pageable);
+        PagedModel<EntityModel<Poll>> collectionModel = pollPagedResourcesAssembler
+                .toModel(polls, pollModelAssembler);
+        return new ResponseEntity<>(collectionModel, HttpStatus.OK);
     }
 
     @ApiOperation(value = "Creates a new Poll", notes = "The newly created poll Id will be sent in the location response header")
@@ -66,8 +79,9 @@ public class PollController {
             @ApiResponse(code = 404, message = "Poll not found", response = ErrorDetails.class),
             @ApiResponse(code = 500, message = "Error searching Poll", response = ErrorDetails.class)})
     @GetMapping("/polls/{pollId}")
-    public ResponseEntity<?> getPoll(@PathVariable Long pollId) {
-        return new ResponseEntity<>(verifyPoll(pollId), HttpStatus.OK);
+    public ResponseEntity<EntityModel<Poll>> getPoll(@PathVariable Long pollId) {
+        Poll verifyPoll = verifyPoll(pollId);
+        return new ResponseEntity<>(pollModelAssembler.toModel(verifyPoll), HttpStatus.OK);
     }
 
     @ApiOperation(value = "Updates an existing poll")
